8b3f7db2ce6d5ceb125e4c9a478c9ba736114e1d
linyanfang <1399217809@qq.com>
2015-06-13 23:54:50 +0800

    add render

--- examples/todo.js
+++ examples/todo.js
@@ -56,7 +56,19 @@ $(function(){
       "click a.destroy" : "clear",
       "keypress .edit" : "updateOnEnter",
       "blur .edit" : "close"
-    },    
+    },  
+
+    initialize: function(){
+      this.listenTo(this.model, 'change', this.render);
+      this.listenTo(this.model, 'destroy', this.remove);
+    },
+
+    render: function(){
+      this.$el.html(this.template(this.model.toJSON()));
+      this.$el.toggleClass('done', this.model.get('done'));
+      this.input = this.$('.edit');
+      return this;
+    },  
 
   })
 })
