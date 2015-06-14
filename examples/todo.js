8ec5c126bb32c8cebd249ed97cd2e2be89c5a46b
linyanfang <1399217809@qq.com>
2015-06-14 23:10:21 +0800

    todo.js finished

--- examples/todo.js
+++ examples/todo.js
@@ -23,7 +23,7 @@ $(function(){
     model: Todo,
 
     //把所有的todo item存放在todos-backbone中
-    localStorage: new Backbone.LocalStorage("todos-backbone");
+    localStorage: new Backbone.LocalStorage("todos-backbone"),
 
     done: function(){
       return this.where({done: true});
@@ -48,7 +48,7 @@ $(function(){
 
     tagName: "li",
 
-    template: _.template($("item-template").html()),
+    template: _.template($("#item-template").html()),
 
     events: {
       "click .toggle" : "toggleDone",
@@ -93,11 +93,67 @@ $(function(){
       if(e.keyCode == 13) this.close();
     },
 
-    close: function(){
+    clear: function(){
       this.model.destroy();
     }
 
   });
+
+  var AppView = Backbone.View.extend({
+
+    el: $("#todoapp"),
+
+    statsTemplate: _.template($('#stats-template').html()),
+
+    events: {
+      "keypress #new-todo": "ceateOnEnter",
+      "click #clear-completed": "clearCompleted",
+      "click #toggle-all": "toggleAllComplete"
+    },
+
+    initialize: function(){
+      this.input = this.$('#new-todo');
+      this.allCheckbox = this.$("#toggle-all")[0];
+      this.listenTo(Todos, 'add', this.addOne);
+      this.listenTo(Todos, 'reset', this.addAll);
+      this.listenTo(Todos, 'all', this.render);
+
+      this.footer = this.$('footer');
+      this.main = $('#main');
+
+      Todos.fetch();
+    },
+
+    addOne: function(todo){
+      var view = new TodoView({model: todo});
+      this.$("#todo-list").append(view.render().el);
+    },
+
+    addAll: function(){
+      Todos.each(this.addOne, this);
+    },
+
+    createOneEnter: function(e){
+      if(e.keyCode != 13) return;
+      if(!this.input.val()) return;
+
+      Todos.create({title: this.input.val()});
+      this.input.val('');
+    },
+
+    clearCompleted: function(){
+      _.invoke(Todo.done(), 'destroy');
+      return false;
+    },
+
+    toggleAllComplete: function(){
+      var done = this.allCheckbox.checked;
+      Todos.each(function(todo){todo.save({'done': done});});
+    }
+
+  });
+
+  var App = new AppView;
 
   
 })
